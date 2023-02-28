031f02d821598abf5993686893ec993f646dff80
Lukas <lukasdaugela197@gmail.com>
2023-02-28 19:08:06 +0200

    Added Arrow svg component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
 import React from 'react';
 
-import Button from './components/Button';
-
 function App() {
   return (
     <>
       <div>App</div>
-      <Button type="gold">Get Free Estimate</Button>
     </>
   );
 }
--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,14 +2,21 @@ import classNames from 'classnames/bind';
 import PropTypes from 'prop-types';
 import React from 'react';
 
+import { Arrow } from '../../../public/assets/svg';
 import styles from './Button.module.scss';
 
 const cn = classNames.bind(styles);
 
 export default function Button({ children, type }) {
-  const customClass = type ? `button__${type}` : '';
+  const customBtnClass = type ? `button__${type}` : '';
+  const customImgClass = type ? `image__${type}` : '';
 
-  return <button className={cn('button', customClass)}>{children}</button>;
+  return (
+    <button className={cn('button', customBtnClass)}>
+      {children}
+      <Arrow className={cn('image', customImgClass)} />
+    </button>
+  );
 }
 
 Button.propTypes = {
