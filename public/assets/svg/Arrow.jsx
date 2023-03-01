37037aac4ce6a7f674bb86bb4bdcb5a2a23342b7
Lukas <lukasdaugela197@gmail.com>
2023-03-01 11:07:48 +0200

    Added media queries to the button component

--- public/assets/svg/Arrow.jsx
+++ public/assets/svg/Arrow.jsx
@@ -4,8 +4,9 @@ import PropTypes from 'prop-types';
 export default function Arrow({ className }) {
   return (
     <svg
-      width="19"
-      height="17"
+      className={className}
+      width="100%"
+      height="100%"
       viewBox="0 0 19 17"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
