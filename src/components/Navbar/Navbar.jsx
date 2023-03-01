def9b01996642e4749e5a07c488c8a81052c301b
Lukas <lukasdaugela197@gmail.com>
2023-03-01 18:57:51 +0200

    Added navbar folders and mobile svgs

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,10 @@
+import classNames from 'classnames/bind';
+import React from 'react';
+
+import styles from './Navbar.module.scss';
+
+const cn = classNames.bind(styles);
+
+export default function Navbar() {
+  return <nav>nav</nav>;
+}
