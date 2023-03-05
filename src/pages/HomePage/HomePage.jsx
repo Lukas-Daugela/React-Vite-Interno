c8653f855d7ed399d8c40c85e26f187164f111b6
Lukas <lukasdaugela197@gmail.com>
2023-03-05 16:18:00 +0200

    Added project card component

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 
 import HomeHero from '../../components/HomeHero';
+import ProjectCard from '../../components/ProjectCard/ProjectCard';
 import MainLayout from '../../layouts/MainLayout';
+import projects from '../../shared/projects.json';
 import heroInfo from './text/heroText.json';
 
 export default function HomePage() {
   return (
     <MainLayout>
       <HomeHero text={heroInfo} />
+      <ProjectCard cardInfo={projects[3]} />
     </MainLayout>
   );
 }
