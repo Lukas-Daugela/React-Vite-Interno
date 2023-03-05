2dc644977ddd19ce80d2c383678f78acc18adac8
Lukas <lukasdaugela197@gmail.com>
2023-03-05 13:56:54 +0200

    Added Testimonial card component

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 
 import HomeHero from '../../components/HomeHero';
+import TestimonialCard from '../../components/TestimonialCard/TestimonialCard';
 import MainLayout from '../../layouts/MainLayout';
 import heroInfo from './text/heroText.json';
+import cardInfo from './text/testimonialsText.json';
 
 export default function HomePage() {
   return (
     <MainLayout>
       <HomeHero text={heroInfo} />
       <div>HomePage</div>
+      <TestimonialCard cardInfo={cardInfo[0]} />
     </MainLayout>
   );
 }
