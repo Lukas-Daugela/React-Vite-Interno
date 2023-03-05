dbb00065e747942d85a4cbc13e96b840eb2711e9
Lukas <lukasdaugela197@gmail.com>
2023-03-05 10:26:13 +0200

    Added news cards info and images

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import HomeHero from '../../components/HomeHero';
 import MainLayout from '../../layouts/MainLayout';
+import testImg from '../../shared/newsCards.json';
 import heroInfo from './text/heroText.json';
 
 export default function HomePage() {
@@ -9,6 +10,9 @@ export default function HomePage() {
     <MainLayout>
       <HomeHero text={heroInfo} />
       <div>HomePage</div>
+      {testImg.map((text) => (
+        <img key={text.imgUrl} src={text.imgUrl} alt="" />
+      ))}
     </MainLayout>
   );
 }
