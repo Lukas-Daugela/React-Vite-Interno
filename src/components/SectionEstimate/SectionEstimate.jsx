8b51b89542aa17a088ef9321abe936ecb668f24e
Lukas <lukasdaugela197@gmail.com>
2023-03-06 12:24:11 +0200

    Added json text file for Estimate section

--- src/components/SectionEstimate/SectionEstimate.jsx
+++ src/components/SectionEstimate/SectionEstimate.jsx
@@ -1,9 +1,7 @@
 import classNames from 'classnames/bind';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import React from 'react';
 import { Link } from 'react-router-dom';
-
-import img from '/assets/images/estimate.jpg';
 
 import { PhoneIcon } from '../../../public/assets/svg';
 import Button from '../Button';
@@ -11,37 +9,32 @@ import styles from './SectionEstimate.module.scss';
 
 const cn = classNames.bind(styles);
 
-export default function SectionEstimate() {
-  // const {} = estimateInfo;
+export default function SectionEstimate({ estimateInfo }) {
+  const { title, description, phone, phoneDescription, buttonText, linkPath, imgUrl } =
+    estimateInfo;
   return (
     <div className={cn('estimate')}>
       <div className={cn('estimate__text-container')}>
-        <h2 className={cn('estimate__title')}>
-          We Create The Art Of Stylish Living Stylishly
-        </h2>
-        <p className={cn('estimate__description')}>
-          It is a long established fact that a reader will be distracted by the of
-          readable content of a page when lookings at its layouts the points of using that
-          it has a more-or-less normal.
-        </p>
+        <h2 className={cn('estimate__title')}>{title}</h2>
+        <p className={cn('estimate__description')}>{description}</p>
         <div className={cn('estimate__phone-container')}>
           <div className={cn('estimate__circle')}>
             <PhoneIcon className={cn('estimate__circle__img')} />
           </div>
           <span className={cn('estimate__phone-text-container')}>
-            <p className={cn('estimate__phone')}>012345678</p>
-            <p className={cn('estimate__phone-text')}>Call Us Anytime</p>
+            <p className={cn('estimate__phone')}>{phone}</p>
+            <p className={cn('estimate__phone-text')}>{phoneDescription}</p>
           </span>
         </div>
-        <Link to="/" className={cn('estimate__button-link')}>
-          <Button>Get Free Estimate</Button>
+        <Link to={linkPath} className={cn('estimate__button-link')}>
+          <Button>{buttonText}</Button>
         </Link>
       </div>
-      <img className={cn('estimate__img')} src={img} alt="" />
+      <img className={cn('estimate__img')} src={imgUrl} alt="" />
     </div>
   );
 }
 
-// SectionEstimate.propTypes = {
-//   estimateInfo: PropTypes.object,
-// };
+SectionEstimate.propTypes = {
+  estimateInfo: PropTypes.object,
+};
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -3,13 +3,13 @@ import React from 'react';
 import HomeHero from '../../components/HomeHero';
 import SectionEstimate from '../../components/SectionEstimate/SectionEstimate';
 import MainLayout from '../../layouts/MainLayout';
-import heroInfo from './text/heroText.json';
+import { estimateText, heroText } from './text';
 
 export default function HomePage() {
   return (
     <MainLayout>
-      <HomeHero text={heroInfo} />
-      <SectionEstimate />
+      <HomeHero text={heroText} />
+      <SectionEstimate estimateInfo={estimateText} />
     </MainLayout>
   );
 }
