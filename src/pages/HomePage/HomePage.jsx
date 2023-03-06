52975ef2f0d97543b353e97a6c13c32709411f38
Lukas <lukasdaugela197@gmail.com>
2023-03-06 13:27:53 +0200

    Added Section services for home page

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 
 import HomeHero from '../../components/HomeHero';
 import SectionEstimate from '../../components/SectionEstimate/SectionEstimate';
+import SectionHomeServices from '../../components/SectionHomeServices';
 import MainLayout from '../../layouts/MainLayout';
+import serviceCardsText from '../../shared/serviceCardsText.json';
 import { estimateText, heroText } from './text';
 
 export default function HomePage() {
   return (
     <MainLayout>
       <HomeHero text={heroText} />
+      <SectionHomeServices cardsText={serviceCardsText} />
       <SectionEstimate estimateInfo={estimateText} />
     </MainLayout>
   );
