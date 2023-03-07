0f4677a04bd0e79584cad5f738229003d0ada1d7
Lukas <lukasdaugela197@gmail.com>
2023-03-07 15:02:01 +0200

    Filled home page, adjusted paddings and naming

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -2,18 +2,16 @@ import React from 'react';
 
 import HomeHero from '../../components/HomeHero';
 import SectionAchievments from '../../components/SectionAchievments';
-import SectionClients from '../../components/SectionClients/SectionClients';
-import SectionEstimate from '../../components/SectionEstimate/SectionEstimate';
+import SectionClients from '../../components/SectionClients';
+import SectionEstimate from '../../components/SectionEstimate';
 import SectionHomeNews from '../../components/SectionHomeNews';
-import SectionHomeProjects from '../../components/SectionHomeProjects/SectionHomeProjects';
+import SectionHomeProjects from '../../components/SectionHomeProjects';
 import SectionHomeServices from '../../components/SectionHomeServices';
+import SectionJoinTeam from '../../components/SectionJoinTeam';
 import SectionTestimonials from '../../components/SectionTestimonials';
 import CustomLayout from '../../layouts/CustomLayout/CustomLayout';
 import MainLayout from '../../layouts/MainLayout';
-import achievments from '../../shared/achievments.json';
-import newsCards from '../../shared/newsCards.json';
-import projects from '../../shared/projects.json';
-import serviceCardsText from '../../shared/serviceCardsText.json';
+import { achievments, newsCards, projects, serviceCards } from '../../shared';
 import { estimateText, heroText, testimonialsText } from './text';
 
 export default function HomePage() {
@@ -21,7 +19,7 @@ export default function HomePage() {
     <>
       <MainLayout>
         <HomeHero text={heroText} />
-        <SectionHomeServices cardsText={serviceCardsText} />
+        <SectionHomeServices cardsText={serviceCards} />
         <SectionEstimate estimateInfo={estimateText} />
         <SectionTestimonials
           testimonialsInfo={testimonialsText}
@@ -33,6 +31,7 @@ export default function HomePage() {
       <CustomLayout>
         <SectionAchievments achievments={achievments} />
         <SectionHomeNews cardsInfo={newsCards} />
+        <SectionJoinTeam />
       </CustomLayout>
     </>
   );
---
+++ src/shared/index.js
@@ -0,0 +1,6 @@
+import achievments from './achievments.json';
+import newsCards from './newsCards.json';
+import projects from './projects.json';
+import serviceCards from './serviceCards.json';
+
+export { achievments, newsCards, projects, serviceCards };
