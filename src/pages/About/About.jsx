d52441cf6cfb9e1dbe3e7cf486e2cd638a24d5c1
Lukas <lukasdaugela197@gmail.com>
2023-03-08 09:13:10 +0200

    Added form mixin, styled Text Input

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
-import TextInputs from '../../components/TextInputs';
+import TextInput from '../../components/TextInput';
 import MainLayout from '../../layouts/MainLayout';
 
 export default function About() {
   return (
     <MainLayout>
       <div>About</div>
-      <TextInputs />
+      <TextInput />
     </MainLayout>
   );
 }
