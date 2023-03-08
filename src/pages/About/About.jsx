c222d1b6a8389a119cd6cca0deeed87981eaf6a9
Lukas <lukasdaugela197@gmail.com>
2023-03-08 09:34:41 +0200

    Fixed Text Area style

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 
-import TextInput from '../../components/TextInput';
 import MainLayout from '../../layouts/MainLayout';
 
 export default function About() {
   return (
     <MainLayout>
       <div>About</div>
-      <TextInput />
     </MainLayout>
   );
 }
