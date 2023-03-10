5c7c92aca9ee9e60e68b16494aa469b716ca9e42
Lukas <lukasdaugela197@gmail.com>
2023-03-10 15:19:47 +0200

    Added Section Message component

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -4,9 +4,9 @@ import React from 'react';
 import heroImg from '/assets/images/heroImages/kitchen-design.jpg';
 
 import Hero from '../../components/Hero/Hero';
-import MessageForm from '../../components/MessageForm/MessageForm';
 import SectionAboutWorkflow from '../../components/SectionAboutWorkflow/SectionAboutWorkflow';
 import SectionCommaV1 from '../../components/SectionCommaV1/SectionCommaV1';
+import SectionMessageForm from '../../components/SectionMessageForm/SectionMessageForm';
 import SectionTeamCards from '../../components/SectionTeamCards/SectionTeamCards';
 import CustomLayout from '../../layouts/CustomLayout/CustomLayout';
 import MainLayout from '../../layouts/MainLayout';
@@ -26,7 +26,11 @@ export default function About() {
       </MainLayout>
       <CustomLayout>
         <SectionTeamCards employeesText={employeesText} />
-        <MessageForm text={formTexts} />
+        <SectionMessageForm
+          formTexts={formTexts}
+          title={"Creative project? Let's have a productive talk."}
+          className={cn('section__message-form')}
+        />
       </CustomLayout>
     </>
   );
