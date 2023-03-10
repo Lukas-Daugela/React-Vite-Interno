fdb7646376cf324459e0881350d71c434734cd3f
Lukas <lukasdaugela197@gmail.com>
2023-03-10 14:57:24 +0200

    Added Custom layout to About page

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -8,6 +8,7 @@ import MessageForm from '../../components/MessageForm/MessageForm';
 import SectionAboutWorkflow from '../../components/SectionAboutWorkflow/SectionAboutWorkflow';
 import SectionCommaV1 from '../../components/SectionCommaV1/SectionCommaV1';
 import SectionTeamCards from '../../components/SectionTeamCards/SectionTeamCards';
+import CustomLayout from '../../layouts/CustomLayout/CustomLayout';
 import MainLayout from '../../layouts/MainLayout';
 import { employeesText } from '../../shared';
 import styles from './About.module.scss';
@@ -17,12 +18,16 @@ const cn = classNames.bind(styles);
 
 export default function About() {
   return (
-    <MainLayout>
-      <Hero title={'About Us'} img={heroImg} />
-      <SectionCommaV1 sectionInfo={commaText} />
-      <SectionAboutWorkflow texts={workflowTexts} className={cn('section-workflow')} />
-      <SectionTeamCards employeesText={employeesText} />
-      <MessageForm text={formTexts} />
-    </MainLayout>
+    <>
+      <MainLayout>
+        <Hero title={'About Us'} img={heroImg} />
+        <SectionCommaV1 sectionInfo={commaText} />
+        <SectionAboutWorkflow texts={workflowTexts} className={cn('section-workflow')} />
+      </MainLayout>
+      <CustomLayout>
+        <SectionTeamCards employeesText={employeesText} />
+        <MessageForm text={formTexts} />
+      </CustomLayout>
+    </>
   );
 }
