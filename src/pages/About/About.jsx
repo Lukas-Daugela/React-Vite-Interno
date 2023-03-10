602a2fbc7342e98d03412b409da3d37ee5521573
Lukas <lukasdaugela197@gmail.com>
2023-03-10 18:27:13 +0200

    Removed unnecessary imports and styles

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,4 +1,3 @@
-import classNames from 'classnames/bind';
 import React from 'react';
 
 import heroImg from '/assets/images/heroImages/kitchen-design.jpg';
@@ -10,9 +9,6 @@ import SectionMessageForm from '../../components/SectionMessageForm/SectionMessa
 import SectionTeamCards from '../../components/SectionTeamCards/SectionTeamCards';
 import CustomLayout from '../../layouts/CustomLayout/CustomLayout';
 import MainLayout from '../../layouts/MainLayout';
-import styles from './About.module.scss';
-
-const cn = classNames.bind(styles);
 
 export default function About() {
   return (
@@ -24,7 +20,7 @@ export default function About() {
       </MainLayout>
       <CustomLayout>
         <SectionTeamCards />
-        <SectionMessageForm className={cn('section__message-form')} />
+        <SectionMessageForm />
       </CustomLayout>
     </>
   );
