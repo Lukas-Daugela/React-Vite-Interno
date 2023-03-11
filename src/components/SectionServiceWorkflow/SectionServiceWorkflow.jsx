7c31416b5c2ddc1aca49f10d19b3e3ee9ff799ec
Lukas <lukasdaugela197@gmail.com>
2023-03-11 22:15:39 +0200

    Added styles and remaining information

--- src/components/SectionServiceWorkflow/SectionServiceWorkflow.jsx
+++ src/components/SectionServiceWorkflow/SectionServiceWorkflow.jsx
@@ -10,23 +10,21 @@ export default function SectionServiceWorkflow() {
   return (
     <div className={cn('workflow')}>
       <h2 className={cn('workflow__title')}>How We Work</h2>
-      <p className={cn('workflow__desciption')}>
+      <p className={cn('workflow__description')}>
         It is a long established fact will be distracted.Lorem Ipsum is simply dummy text
         of the printing and typesetting industry.
       </p>
       <div className={cn('workflow-steps')}>
         {texts.map((text) => (
           <div key={text.id} className={cn('workflow-step')}>
-            <img src={text.imgUrl} alt="" className={cn('workflow-step')} />
+            <img src={text.imgUrl} alt="" className={cn('workflow-step__image')} />
             <div className={cn('text-side')}>
               <div className={cn('text-side__icon-container')}>
                 <img src={text.iconUrl} alt="" />
-                <p className={cn('text-side__icon-container')}>{text.step}</p>
+                <p className={cn('text-side__number')}>{text.step}</p>
               </div>
-              <div className={cn('text-side__text-container')}>
-                <h3>{text.title}</h3>
-                <p>{text.description}</p>
-              </div>
+              <h3 className={cn('text-side__title')}>{text.title}</h3>
+              <p className={cn('text-side__description')}>{text.description}</p>
             </div>
           </div>
         ))}
