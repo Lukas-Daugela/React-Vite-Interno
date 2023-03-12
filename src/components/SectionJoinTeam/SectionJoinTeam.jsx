03ffafaee45778bda2d0af7f7884390e9526e525
Lukas <lukasdaugela197@gmail.com>
2023-03-12 14:19:27 +0200

    Added and adjusted Section Join Team

--- src/components/SectionJoinTeam/SectionJoinTeam.jsx
+++ src/components/SectionJoinTeam/SectionJoinTeam.jsx
@@ -1,4 +1,5 @@
 import classNames from 'classnames/bind';
+import PropTypes from 'prop-types';
 import React from 'react';
 
 import Button from '../Button';
@@ -6,9 +7,9 @@ import styles from './SectionJoinTeam.module.scss';
 
 const cn = classNames.bind(styles);
 
-export default function SectionJoinTeam() {
+export default function SectionJoinTeam({ className }) {
   return (
-    <div className={cn('join-team')}>
+    <div className={cn('join-team', className)}>
       <h2 className={cn('join-team__title')}>Wanna join the interno?</h2>
       <p className={cn('join-team__description')}>
         It is a long established fact will be distracted.
@@ -19,3 +20,7 @@ export default function SectionJoinTeam() {
     </div>
   );
 }
+
+SectionJoinTeam.propTypes = {
+  className: PropTypes.string,
+};
--- src/pages/Services/Services.jsx
+++ src/pages/Services/Services.jsx
@@ -1,11 +1,16 @@
+import classNames from 'classnames/bind';
 import React from 'react';
 
 import heroImg from '/assets/images/heroImages/modern-kitchen.jpg';
 
 import Hero from '../../components/Hero';
+import SectionJoinTeam from '../../components/SectionJoinTeam';
 import SectionServices from '../../components/SectionServices';
 import SectionServiceWorkflow from '../../components/SectionServiceWorkflow';
 import MainLayout from '../../layouts/MainLayout';
+import styles from './Services.module.scss';
+
+const cn = classNames.bind(styles);
 
 export default function Services() {
   return (
@@ -13,6 +18,7 @@ export default function Services() {
       <Hero title={'Services'} img={heroImg} />
       <SectionServices />
       <SectionServiceWorkflow />
+      <SectionJoinTeam className={cn('join-team')} />
     </MainLayout>
   );
 }
