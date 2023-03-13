79ce1873ae9261d7829c33856b5c0469560ddf00
Lukas <lukasdaugela197@gmail.com>
2023-03-13 15:52:14 +0200

    Added Ordered list component for less code

--- src/components/SectionServiceBenefits/SectionServiceBenefits.jsx
+++ src/components/SectionServiceBenefits/SectionServiceBenefits.jsx
@@ -1,36 +1,16 @@
 import classNames from 'classnames/bind';
 import React from 'react';
 
+import BenefitsOrderedList from '../BenefitsOrderedList/BenefitsOrderedList';
 import styles from './SectionServiceBenefits.module.scss';
 import benefitLists from './texts/benefitLists.json';
 
 const cn = classNames.bind(styles);
 
 export default function SectionServiceBenefits() {
-  const { firstList, secondList } = benefitLists;
-
   return (
     <div className={cn('benefits')}>
-      <div className={cn('benefits__list-container')}>
-        <h2 className={cn('benefits__list-title')}>{firstList.title}</h2>
-        <ol className={cn('ordered-list')}>
-          {firstList.items.map((listItem, i) => (
-            <li key={i} className={cn('ordered-list__item')}>
-              <p className={cn('ordered-list__text')}>{listItem}</p>
-            </li>
-          ))}
-        </ol>
-      </div>
-      <div className={cn('benefits__list-container')}>
-        <h2 className={cn('benefits__list-title')}>{secondList.title}</h2>
-        <ol className={cn('ordered-list')}>
-          {secondList.items.map((listItem, i) => (
-            <li key={i} className={cn('ordered-list__item')}>
-              <p className={cn('ordered-list__text')}>{listItem}</p>
-            </li>
-          ))}
-        </ol>
-      </div>
+      <BenefitsOrderedList benefitLists={benefitLists} />
     </div>
   );
 }
