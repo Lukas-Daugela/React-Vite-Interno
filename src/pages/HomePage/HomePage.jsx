da1a3220dcfe6a812693ac9a5de987abb86e9a8d
Lukas <lukasdaugela197@gmail.com>
2023-03-13 17:25:47 +0200

    Deleted unnecessary lines

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,8 +1,6 @@
 import classNames from 'classnames/bind';
 import React from 'react';
 
-import thumbnailImg from '/assets/images/services/minimal-work-space.jpg';
-
 import HomeHero from '../../components/HomeHero';
 import SectionAchievments from '../../components/SectionAchievments';
 import SectionClients from '../../components/SectionClients';
@@ -12,7 +10,6 @@ import SectionHomeProjects from '../../components/SectionHomeProjects';
 import SectionHomeServices from '../../components/SectionHomeServices';
 import SectionJoinTeam from '../../components/SectionJoinTeam';
 import SectionTestimonials from '../../components/SectionTestimonials';
-import VideoModal from '../../components/VideoModal/VideoModal';
 import CustomLayout from '../../layouts/CustomLayout/CustomLayout';
 import MainLayout from '../../layouts/MainLayout';
 import styles from './HomePage.module.scss';
@@ -24,10 +21,6 @@ export default function HomePage() {
     <>
       <MainLayout>
         <HomeHero />
-        <VideoModal
-          thumbnail={thumbnailImg}
-          videoUrl={'https://www.youtube.com/embed/L93hyPiltLA'}
-        />
         <SectionHomeServices />
         <SectionEstimate />
         <SectionTestimonials />
