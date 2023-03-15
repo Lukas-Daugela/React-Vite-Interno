1abdadfa493fd8b4096720e6c5cfd8a716269756
Lukas <lukasdaugela197@gmail.com>
2023-03-15 15:56:04 +0200

    Expanded projects info files with new structure

---
+++ src/components/SectionProjects/projectsInfo/index.js
@@ -0,0 +1,6 @@
+import bedroom from './bedroom.json';
+import bathroom from './bathroom.json';
+import kitchan from './kitchan.json';
+import livingroom from './livingroom.json';
+
+export { bedroom, bathroom, kitchan, livingroom };
---
+++ src/shared/projects.js
@@ -0,0 +1,25 @@
+import {
+  bedroom,
+  bathroom,
+  kitchan,
+  livingroom,
+} from '../components/SectionProjects/projectsInfo';
+
+export const projectss = [
+  {
+    category: 'bedroom',
+    projectsInfo: bedroom,
+  },
+  {
+    category: 'bathroom',
+    projectsInfo: bathroom,
+  },
+  {
+    category: 'kitchan',
+    projectsInfo: kitchan,
+  },
+  {
+    category: 'livingroom',
+    projectsInfo: livingroom,
+  },
+];
