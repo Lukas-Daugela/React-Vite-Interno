da9fc225bc3411793202f4116abc44bcbc9f90fb
Lukas <lukasdaugela197@gmail.com>
2023-03-22 12:17:23 +0200

    Updated styles

--- src/pages/Blogs/Blogs.jsx
+++ src/pages/Blogs/Blogs.jsx
@@ -1,16 +1,11 @@
 import React from 'react';
 
-import SectionError from '../../components/SectionError/SectionError';
 import MainLayout from '../../layouts/MainLayout';
 
 export default function Blogs() {
   return (
-    <SectionError
-      blurhash={'UiIhW[D%Rjt7~qaeNFa#IpWBWBofM{NGt7of'}
-      buttonText={'Back To Home'}
-      description={'We are sorry, but the page you requested was not found'}
-      imgUrl={'/assets/images/messy-room.jpg'}
-      title={'404'}
-    />
+    <MainLayout>
+      <div>Blogs</div>
+    </MainLayout>
   );
 }
