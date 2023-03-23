68fdb3cac0a05940e7642abdf2432c4972791a3a
Lukas <lukasdaugela197@gmail.com>
2023-03-23 19:46:52 +0200

    Added After submit message component

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,14 +1,16 @@
 import classNames from 'classnames/bind';
 import { Form, Formik } from 'formik';
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useState } from 'react';
 
 import {
   contactCheckboxValidationSchema,
   contactValidationSchema,
 } from '../../shared/form/validation/validationSchema';
+import AfterSubmitMessage from '../AfterSubmitMessage/AfterSubmitMessage';
 import Button from '../Button';
 import Checkbox from '../Checkbox';
+import Modal from '../Modal';
 import TextArea from '../TextArea';
 import TextInput from '../TextInput';
 import styles from './ContactForm.module.scss';
@@ -16,6 +18,8 @@ import styles from './ContactForm.module.scss';
 const cn = classNames.bind(styles);
 
 export default function ContactForm({ text, checkboxText }) {
+  const [showMessage, setShowMessage] = useState(false);
+
   const btnWrapperCustomClass = checkboxText
     ? 'contact-form__button-wrapper--with-checkbox'
     : 'contact-form__button-wrapper';
@@ -37,54 +41,66 @@ export default function ContactForm({ text, checkboxText }) {
 
   const handleOnSubmit = (values, { resetForm }) => {
     console.log('Form values', values);
+    setShowMessage(true);
     resetForm();
   };
 
+  const handleModalOnClose = () => {
+    setShowMessage(false);
+  };
+
   return (
-    <Formik
-      initialValues={initialValues}
-      validationSchema={validationSchema}
-      onSubmit={handleOnSubmit}
-    >
-      {({ handleSubmit }) => (
-        <Form className={cn('contact-form')}>
-          <div className={cn('contact-form__container')}>
-            <TextInput
-              name="name"
-              autoComplete="given-name"
-              type="text"
-              placeholder={name.placeholder}
-            />
-            <TextInput
-              name="email"
-              autoComplete="email"
-              type="text"
-              placeholder={email.placeholder}
-            />
-            <TextInput name="subject" type="text" placeholder={subject.placeholder} />
-            <TextInput
-              name="phone"
-              autoComplete="tel"
-              type="tel"
-              placeholder={phone.placeholder}
-            />
-          </div>
-          <TextArea name="message" placeholder={message.placeholder} />
-          {checkboxText && (
-            <Checkbox
-              name="privacy"
-              label={checkboxText}
-              className={cn('contact-form__checkbox')}
-            />
-          )}
-          <div className={cn(btnWrapperCustomClass)}>
-            <Button type={'submit'} onClick={handleSubmit}>
-              Send Now
-            </Button>
-          </div>
-        </Form>
+    <>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={handleOnSubmit}
+      >
+        {({ handleSubmit }) => (
+          <Form className={cn('contact-form')}>
+            <div className={cn('contact-form__container')}>
+              <TextInput
+                name="name"
+                autoComplete="given-name"
+                type="text"
+                placeholder={name.placeholder}
+              />
+              <TextInput
+                name="email"
+                autoComplete="email"
+                type="text"
+                placeholder={email.placeholder}
+              />
+              <TextInput name="subject" type="text" placeholder={subject.placeholder} />
+              <TextInput
+                name="phone"
+                autoComplete="tel"
+                type="tel"
+                placeholder={phone.placeholder}
+              />
+            </div>
+            <TextArea name="message" placeholder={message.placeholder} />
+            {checkboxText && (
+              <Checkbox
+                name="privacy"
+                label={checkboxText}
+                className={cn('contact-form__checkbox')}
+              />
+            )}
+            <div className={cn(btnWrapperCustomClass)}>
+              <Button type={'submit'} onClick={handleSubmit}>
+                Send Now
+              </Button>
+            </div>
+          </Form>
+        )}
+      </Formik>
+      {showMessage && (
+        <Modal ariaLabel="testing" isOpen={showMessage} onClose={handleModalOnClose}>
+          <AfterSubmitMessage />
+        </Modal>
       )}
-    </Formik>
+    </>
   );
 }
 
