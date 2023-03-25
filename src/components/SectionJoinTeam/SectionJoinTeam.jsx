016e3a50b6ab2499c7296f5dd0cc942d8ef8bc13
Lukas <lukasdaugela197@gmail.com>
2023-03-25 08:19:02 +0200

    Updated Section Join Team

--- src/components/SectionJoinTeam/SectionJoinTeam.jsx
+++ src/components/SectionJoinTeam/SectionJoinTeam.jsx
@@ -1,6 +1,7 @@
 import classNames from 'classnames/bind';
 import PropTypes from 'prop-types';
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import Button from '../Button';
 import styles from './SectionJoinTeam.module.scss';
@@ -8,14 +9,22 @@ import styles from './SectionJoinTeam.module.scss';
 const cn = classNames.bind(styles);
 
 export default function SectionJoinTeam({ className }) {
+  const navigate = useNavigate();
+
+  const handleNavigate = () => {
+    navigate('contact');
+  };
+
   return (
     <div className={cn('join-team', className)}>
       <h2 className={cn('join-team__title')}>Wanna join the interno?</h2>
       <p className={cn('join-team__description')}>
-        It is a long established fact will be distracted.
+        We are always looking for new talented colleagues.
       </p>
       <div className={cn('join-team__button-container')}>
-        <Button styleType={'gold'}>Contact With Us</Button>
+        <Button styleType={'gold'} onClick={handleNavigate}>
+          Contact With Us
+        </Button>
       </div>
     </div>
   );
