d1e7d06f46256f418333adee8c0c10b4694ae5ac
Lukas <lukasdaugela197@gmail.com>
2023-03-31 16:12:02 +0300

    Added Hero

--- src/pages/Blogs/Blogs.jsx
+++ src/pages/Blogs/Blogs.jsx
@@ -1,13 +1,17 @@
+import Hero from '@components/molecules/Hero';
 import MainLayout from '@layouts/MainLayout';
+import heroImages from '@shared/lazyLoadImages/heroImages.json';
 import React from 'react';
 
 import SectionBlogsIntro from './components/SectionBlogsIntro';
 import SectionBlogsNews from './components/SectionBlogsNews';
 
 export default function Blogs() {
+  const { blogs } = heroImages;
+
   return (
     <MainLayout>
-      <div>Blogs</div>
+      <Hero title={'Articles & News'} img={blogs.image} blurhash={blogs.blurhash} />
       <SectionBlogsIntro />
       <SectionBlogsNews />
     </MainLayout>
